60015d9524310a870a8321fb978b5f7a3a1cbc53
shutu <huangshutu2163.com>
2022-04-17 01:28:10 +0800

    提交

--- spring-cloud-client/src/main/java/com/alibaba/client/controller/ClientController.java
+++ spring-cloud-client/src/main/java/com/alibaba/client/controller/ClientController.java
@@ -20,9 +20,6 @@ public class ClientController {
     @Autowired
     private IClientService clientService;
 
-
-
-    //
     @PostMapping("/query")
     public String queryClient() {
             return clientService.queryClient();
