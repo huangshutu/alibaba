9601274366392f1349eb79f4458bc2931dd065e9
shutu <huangshutu@163.com>
2022-04-23 21:05:32 +0800

    修改

--- spring-cloud-server/src/main/java/com/alibaba/server/controller/ServerController.java
+++ spring-cloud-server/src/main/java/com/alibaba/server/controller/ServerController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-;
-
 /**
  * @description:
  * @author: shutu
